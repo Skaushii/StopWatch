09716dabe6dac6fc70a9e688644f058658c253b0
Skaushii <kaushikadhi08@gmail.com>
2024-09-06 20:05:08 +0530

    stop

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect,useState } from 'react';
 
 function App() {
+  const[timer,settimer]=useState(0);
+  const[running,setrunning]=useState(false);
+
+  useEffect(() => {
+    let timeinterval;
+    if(running){
+      timeinterval=setInterval(()=> {
+        settimer((prevTime)=>prevTime + 10);
+    },10);
+  }else if(!running){
+    clearInterval(timeinterval);
+  }
+  return ()=> clearInterval(timeinterval);
+  }, [running]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <h1>STOP WATCH</h1>
+    <div className='cal'>
+    <h1>
+    <span>{String(Math.floor(timer / 3600)).padStart(2, '0')}:</span>
+    <span>{String(Math.floor((timer % 3600) / 60)).padStart(2, '0')}:</span>
+    <span>{String(timer % 60).padStart(2, '0')}</span>
+    </h1>
     </div>
+    <div className='buttons'>
+      <button onClick={()=> setrunning(true)}>Start</button>
+      <button onClick={()=> setrunning(false)}>Stop</button>
+      <button onClick={()=> settimer(0)}>Reset</button>
+
+    </div>
+    </>
   );
 }
 
